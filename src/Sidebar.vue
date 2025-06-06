36cc3363eea8bfaff3269eb32d9492935e651ca9
Th0mmi3 <51088647+Th0mmi3@users.noreply.github.com>
2025-06-06 21:20:31 +0200

    initial commit

---
+++ src/Sidebar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="sidebar">
+    </div>
+</template>
+
+<style scoped>
+.sidebar {
+  width: 100px;
+  height: 100vh;
+  background: rgba(72, 73, 82, 0.34);
+  border-radius: 1px;
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(7.1px);
+  -webkit-backdrop-filter: blur(7.1px);
+  border: 1px solid rgba(244, 240, 255, 0.13);
+}
+</style>
